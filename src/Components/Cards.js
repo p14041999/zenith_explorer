dea06e732bec38b7c1da01d1efb1a48b64f7b533
p14041999 <p14041999@gmail.com>
2021-09-06 22:15:19 +0530

    Update Cards.js

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -10,27 +10,16 @@ class Cards extends Component {
             <h3>Circulating Supply</h3>
             <h2 className="text-primary">80M</h2>
             <span>/100M</span>
-//             <p>
-//               <span className="text-primary">80%</span> is circulating
-//             </p>
+
           </div>
           <div className="card darkBack p-1 px-3 text-light">
             <h3>Total Holders</h3>
             <h2 className="text-primary">1006</h2>
-//             <span>/21M</span>
-//             <p>
-//               Delinquent stake:{""}
-//               <span className="text-primary">50.6%</span>
-//             </p>
           </div>
           <div className="card darkBack p-1 px-3 text-light">
             <h3>Price</h3>
             <h2 className="text-primary">$0.2</h2>
             {/* <span>/21M</span> */}
-//             <p>
-//               MCap: <span className="text-primary">55.8%</span>
-//               is circulating
-//             </p>
           </div>
         </div>
       </div>
