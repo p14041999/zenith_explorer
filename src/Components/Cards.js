e2299fbd2a874a1e2d97c832ec4cb83997010926
p14041999 <p14041999@gmail.com>
2021-09-06 22:19:12 +0530

    Update Cards.js

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -8,24 +8,24 @@ class Cards extends Component {
         <div className="card-cont py-2 mcontainer row">
           <div className="card darkBack p-1 px-3 text-light">
             <h3>Circulating Supply</h3>
-            <h2 className="text-primary">1M</h2>
-            <span>/21M</span>
+            <h2 className="text-primary">80M</h2>
+            <span>/100M</span>
             {/* <p>
               <span className="text-primary">55.8%</span> is circulating
             </p> */}
           </div>
           <div className="card darkBack p-1 px-3 text-light">
             <h3>Total Holdings</h3>
-            <h2 className="text-primary">1000</h2>
-            <span>/21M</span>
-            {/* <p>
+            <h2 className="text-primary">1006</h2>
+            {/*<span>/21M</span>
+             <p>
               Delinquent stake:{""}
               <span className="text-primary">50.6%</span>
             </p> */}
           </div>
           <div className="card darkBack p-1 px-3 text-light">
             <h3>Price</h3>
-            <h2 className="text-primary">$1.800</h2>
+            <h2 className="text-primary">$0.2</h2>
             {/* <span>/21M</span> */}
             {/* <p>
               MCap: <span className="text-primary">55.8%</span>
